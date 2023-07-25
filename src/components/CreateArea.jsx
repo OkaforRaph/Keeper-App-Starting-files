909f25c95267452c16acb8b59e102f5f483f27d2
Okafor Raphael <95545345+OkaforRaph@users.noreply.github.com>
2023-07-25 09:07:08 +0100

    Initial commit

---
+++ src/components/CreateArea.jsx
@@ -0,0 +1,51 @@
+import React, { useState } from "react";
+
+function CreateArea(props) {
+  const [note, setNote] = useState({
+    title: "",
+    content: ""
+  });
+
+  function handleChange(event) {
+    const { name, value } = event.target;
+
+    setNote((prevNote) => {
+      return {
+        ...prevNote,
+        [name]: value // This assigns the value to the name
+      };
+    });
+  }
+
+  function submitNote(event) {
+    props.onAdd(note); //Passing the currently created note, and call the onadd function in the app.jsx file
+    setNote({
+      title: "",
+      content: ""
+    });
+    event.preventDefault(); // Prevents the page from refeshing when the button is clicked
+  }
+
+  return (
+    <div>
+      <form>
+        <input
+          name="title"
+          onChange={handleChange}
+          value={note.title}
+          placeholder="Title"
+        />
+        <textarea
+          name="content"
+          onChange={handleChange}
+          value={note.content}
+          placeholder="Take a note..."
+          rows="3"
+        />
+        <button onClick={submitNote}>Add</button>
+      </form>
+    </div>
+  );
+}
+
+export default CreateArea;
